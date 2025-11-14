a4440c0a352b7b7964283858f88aa1030c287bc1
Luka Maric <lukamaric718@gmail.com>
2025-11-14 19:14:40 +0100

    Added separate DTOs for person and updating person profile

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/PersonProfileDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.API.Dtos
+{
+    internal class PersonProfileDto
+    {
+        public long UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string? Biography { get; set; }
+        public string? Motto { get; set; }
+        public string? ProfileImageUrl { get; set; }
+    }
+}
