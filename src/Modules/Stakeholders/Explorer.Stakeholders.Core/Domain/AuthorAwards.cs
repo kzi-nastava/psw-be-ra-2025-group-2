5433df444ef3e2a2df88ecd6e4f57109c8b56050
Vladimir Zoric <vladimir.zorke@gmail.com>
2025-11-17 06:15:54 +0100

    fix: AuthorAwards current year validation

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/AuthorAwards.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/AuthorAwards.cs
@@ -32,9 +32,7 @@ namespace Explorer.Stakeholders.Core.Domain
         private void Validate()
         {
             if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentException("Invalid awards name.");
-            if (Year < DateTime.Today.Year) throw new ArgumentException("Invalid year");
             if (!Enum.IsDefined(typeof(AwardsState), State)) throw new ArgumentException("Invalid awards state.");
-            if (VotingStartDate.Year < DateTime.Today.Year || VotingEndDate.Year < DateTime.Today.Year) throw new ArgumentException("Invalid voting date.");
             if (VotingStartDate > VotingEndDate) throw new ArgumentException("Invalid voting dates.");
         }
     }
