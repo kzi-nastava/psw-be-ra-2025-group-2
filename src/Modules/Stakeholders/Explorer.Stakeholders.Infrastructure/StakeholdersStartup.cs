298f2d957ff069bfd6a1a67eda011cb7d124a2e7
Anđela Pinjić <pinjic.a276@gmail.com>
2025-11-18 23:54:33 +0100

    feat: configure TourPreferences DbContext, repository and DI

--- src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/StakeholdersStartup.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/StakeholdersStartup.cs
@@ -26,12 +26,14 @@ public static class StakeholdersStartup
     {
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddScoped<ITokenGenerator, JwtGenerator>();
+        services.AddScoped<ITourPreferencesService, TourPreferencesService>();
     }
 
     private static void SetupInfrastructure(IServiceCollection services)
     {
         services.AddScoped<IPersonRepository, PersonDbRepository>();
         services.AddScoped<IUserRepository, UserDbRepository>();
+        services.AddScoped<ITourPreferencesRepository, TourPreferencesDbRepository>();
 
         var dataSourceBuilder = new NpgsqlDataSourceBuilder(DbConnectionStringBuilder.Build("stakeholders"));
         dataSourceBuilder.EnableDynamicJson();
