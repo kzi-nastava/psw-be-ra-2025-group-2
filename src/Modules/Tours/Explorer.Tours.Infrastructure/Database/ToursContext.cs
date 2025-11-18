fe0220f989c53848d23f1c42aa46093bf1fdbdb0
Igor Ludajic <igor.ludajic@gmail.com>
2025-11-18 18:58:35 +0100

    feat: added Monument Controller and DbRepository

--- src/Modules/Tours/Explorer.Tours.Infrastructure/Database/ToursContext.cs
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/Database/ToursContext.cs
@@ -7,8 +7,9 @@ namespace Explorer.Tours.Infrastructure.Database;
 public class ToursContext : DbContext
 {
     public DbSet<Equipment> Equipment { get; set; }
+    public DbSet<Monument> Monument { get; set; }
 
-    public ToursContext(DbContextOptions<ToursContext> options) : base(options) {}
+    public ToursContext(DbContextOptions<ToursContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
