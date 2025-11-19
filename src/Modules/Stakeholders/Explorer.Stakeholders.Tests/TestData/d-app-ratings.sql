684c39f854d1a6c7c2ecfea464416226af22ec34
unknown <bojanamilosevic.ns@gmail.com>
2025-11-19 22:16:20 +0100

    Changed test data

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-app-ratings.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-app-ratings.sql
@@ -1,4 +1,4 @@
 ﻿INSERT INTO stakeholders."AppRatings"("Id", "UserId", "Score", "Comment", "CreatedAt", "UpdatedAt")
 VALUES 
-    (-1, -21, 5, 'Ovo je Perina ocena.', '2025-01-01T12:00:00Z', null),
-    (-2, -22, 3, 'Ovo je Mikina ocena.', '2025-01-01T12:05:00Z', null);
+    (-1, -21, 5, 'The app works well.', '2025-01-01T12:00:00Z', null),
+    (-2, -22, 3, 'Overall, a decent experience.', '2025-01-01T12:05:00Z', null);
