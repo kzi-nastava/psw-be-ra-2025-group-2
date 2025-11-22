38d17c8260c06365f0f8762d487c74518a8c6dea
Luka Maric <lukamaric718@gmail.com>
2025-11-22 18:51:25 +0100

    Added tests for database integration

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/Integration/People/PersonCommandTests.cs
@@ -0,0 +1,112 @@
+﻿using Explorer.API.Controllers.Stakeholders;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Explorer.Stakeholders.Infrastructure.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Explorer.Stakeholders.Tests.Integration.People
+{
+    [Collection("Sequential")]
+    public class PersonCommandTests : BaseStakeholdersIntegrationTest
+    {
+        public PersonCommandTests(StakeholdersTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void Updates_own_profile()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope, "-100");
+            var dbContext = scope.ServiceProvider.GetRequiredService<StakeholdersContext>();
+
+            var updatedProfile = new UpdatePersonProfileDto
+            {
+                Biography = "Nova biografija iz testa",
+                Motto = "Test motto",
+                ProfileImageUrl = "https://test.com/image.jpg"
+            };
+
+            // Act
+            var result = ((ObjectResult)controller.UpdateProfile(updatedProfile).Result)?.Value as PersonProfileDto;
+
+            // Assert - Response
+            result.ShouldNotBeNull();
+            result.Biography.ShouldBe(updatedProfile.Biography);
+            result.Motto.ShouldBe(updatedProfile.Motto);
+            result.ProfileImageUrl.ShouldBe(updatedProfile.ProfileImageUrl);
+            result.Name.ShouldBe("Test");
+            result.Surname.ShouldBe("User");
+            result.Email.ShouldBe("testuser1@test.com");
+
+            // Assert - Database
+            var storedEntity = dbContext.People.FirstOrDefault(p => p.UserId == -100);
+            storedEntity.ShouldNotBeNull();
+            storedEntity.Biography.ShouldBe(updatedProfile.Biography);
+        }
+
+        [Fact]
+        public void Updates_profile_with_null_values()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope, "-100");
+            var dbContext = scope.ServiceProvider.GetRequiredService<StakeholdersContext>();
+
+            var updatedProfile = new UpdatePersonProfileDto
+            {
+                Biography = null,
+                Motto = null,
+                ProfileImageUrl = null
+            };
+
+            // Act
+            var result = ((ObjectResult)controller.UpdateProfile(updatedProfile).Result)?.Value as PersonProfileDto;
+
+            // Assert - Response
+            result.ShouldNotBeNull();
+            result.Biography.ShouldBeNull();
+            result.Motto.ShouldBeNull();
+            result.ProfileImageUrl.ShouldBeNull();
+
+            // Assert - Database
+            var storedEntity = dbContext.People.FirstOrDefault(p => p.UserId == -100);
+            storedEntity.ShouldNotBeNull();
+            storedEntity.Biography.ShouldBeNull();
+            storedEntity.Motto.ShouldBeNull();
+            storedEntity.ProfileImageUrl.ShouldBeNull();
+        }
+
+        [Fact]
+        public void Updates_only_biography()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope, "-101");
+            var dbContext = scope.ServiceProvider.GetRequiredService<StakeholdersContext>();
+
+            var updatedProfile = new UpdatePersonProfileDto
+            {
+                Biography = "Nova biografija",
+                Motto = null,
+                ProfileImageUrl = null
+            };
+
+            // Act
+            var result = ((ObjectResult)controller.UpdateProfile(updatedProfile).Result)?.Value as PersonProfileDto;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.Biography.ShouldBe("Nova biografija");
+        }
+
+        private static PersonController CreateController(IServiceScope scope, string userId)
+        {
+            return new PersonController(scope.ServiceProvider.GetRequiredService<IPersonService>())
+            {
+                ControllerContext = BuildContext(userId)
+            };
+        }
+    }
+}
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/Integration/People/PersonQueryTests.cs
@@ -0,0 +1,71 @@
+﻿using Explorer.API.Controllers.Stakeholders;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Explorer.Stakeholders.Tests.Integration.People
+{
+    [Collection("Sequential")]
+    public class PersonQueryTests : BaseStakeholdersIntegrationTest
+    {
+        public PersonQueryTests(StakeholdersTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void Retrieves_profile_for_testuser1()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope, "-100");
+
+            var result = ((ObjectResult)controller.GetProfile().Result)?.Value as PersonProfileDto;
+
+            result.ShouldNotBeNull();
+            result.UserId.ShouldBe(-100);
+            result.Name.ShouldBe("Test");
+            result.Surname.ShouldBe("User");
+            result.Email.ShouldBe("testuser1@test.com");
+        }
+
+        [Fact]
+        public void Retrieves_profile_for_testuser2()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope, "-101");
+
+            var result = ((ObjectResult)controller.GetProfile().Result)?.Value as PersonProfileDto;
+
+            result.ShouldNotBeNull();
+            result.UserId.ShouldBe(-101);
+            result.Name.ShouldBe("Test");
+            result.Surname.ShouldBe("User2");
+            result.Email.ShouldBe("testuser2@test.com");
+            result.Biography.ShouldBe("Existing bio");
+            result.Motto.ShouldBe("Old motto");
+            result.ProfileImageUrl.ShouldBe("http://old.jpg");
+        }
+
+        [Fact]
+        public void Retrieves_profile_for_testauthor()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope, "-102");
+
+            var result = ((ObjectResult)controller.GetProfile().Result)?.Value as PersonProfileDto;
+
+            result.ShouldNotBeNull();
+            result.UserId.ShouldBe(-102);
+            result.Name.ShouldBe("Test");
+            result.Surname.ShouldBe("Author");
+            result.Email.ShouldBe("testauthor@test.com");
+        }
+
+        private static PersonController CreateController(IServiceScope scope, string userId)
+        {
+            return new PersonController(scope.ServiceProvider.GetRequiredService<IPersonService>())
+            {
+                ControllerContext = BuildContext(userId)
+            };
+        }
+    }
+}
