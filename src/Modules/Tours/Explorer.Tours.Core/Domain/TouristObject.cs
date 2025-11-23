94e1e81126233cc11174a99bb3f85a0b335c8102
Boriša Hrnjez <borisahrnjez@gmail.com>
2025-11-23 22:44:05 +0100

    fix: coordinates validation

--- src/Modules/Tours/Explorer.Tours.Core/Domain/TouristObject.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TouristObject.cs
@@ -22,8 +22,8 @@ public class TouristObject : Entity
     private void Validate()
     {
         if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentException("Name cannot be empty.");
-        if (Latitude < 0 || Latitude > 90) throw new ArgumentException("Latitude must be between 0 and 90 degrees.");
-        if (Longitude < 0 || Longitude > 180) throw new ArgumentException("Longitude must be between 0 and 180 degrees.");
+        if (Latitude < -90 || Latitude > 90) throw new ArgumentException("Latitude must be between -90 and 90 degrees.");
+        if (Longitude < -180 || Longitude > 180) throw new ArgumentException("Longitude must be between -180 and 180 degrees.");
     }
 
 }
