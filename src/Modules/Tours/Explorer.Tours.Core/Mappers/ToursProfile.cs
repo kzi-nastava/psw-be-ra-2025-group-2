4e2a501ef02112cd4e96c6ad420b6765c75f83bc
Vladimir Zoric <vladimir.zorke@gmail.com>
2025-11-26 10:30:09 +0100

    fix: remove duplicate TouristEquipment mapping

--- src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
@@ -17,12 +17,6 @@ public class ToursProfile : Profile
 
         CreateMap<TourProblem, TourProblemDto>().ReverseMap();
         CreateMap<CreateTourProblemDto, TourProblem>();
-        CreateMap<TouristEquipment, TouristEquipmentDto>()
-            .ForMember(dest => dest.Equipment, opt => opt.MapFrom(src => src.Equipment))
-            .ForMember(dest => dest.Equipments, opt => opt.Ignore());
-
-        CreateMap<TouristEquipmentDto, TouristEquipment>()
-            .ForMember(dest => dest.Equipment, opt => opt.MapFrom(src => src.Equipment));
 
         CreateMap<TouristObject, TouristObjectDto>()
             .ForMember(
