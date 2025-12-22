19de33290e19145365f563258d00cba98d033b27
Vladimir Zoric <vladimir.zorke@gmail.com>
2025-12-22 18:54:33 +0100

    feat: add Encounters module

---
+++ src/Modules/Encounters/Explorer.Encounters.Infrastructure/EncountersStartup.cs
@@ -0,0 +1,44 @@
+﻿using Explorer.BuildingBlocks.Infrastructure.Database;
+using Explorer.Encounters.Core.Mappers;
+using Explorer.Encounters.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.Infrastructure
+{
+    public static class EncountersStartup
+    {
+        public static IServiceCollection ConfigureEncountersModule(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(EncountersProfile).Assembly);
+            SetupCore(services);
+            SetupInfrastructure(services);
+            return services;
+        }
+
+        private static void SetupCore(IServiceCollection services)
+        {
+            // TODO ubaciti servise
+        }
+
+        private static void SetupInfrastructure(IServiceCollection services)
+        {
+            // TODO ubaciti servise
+
+            var dataSourceBuilder = new NpgsqlDataSourceBuilder(DbConnectionStringBuilder.Build("encounters"));
+            dataSourceBuilder.EnableDynamicJson();
+
+            var dataSource = dataSourceBuilder.Build();
+
+            services.AddDbContext<EncountersContext>(opt =>
+            opt.UseNpgsql(dataSource, x => x.MigrationsHistoryTable("__EFMigrationsHistory", "encounters")));
+        }
+    }
+}
