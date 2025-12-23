53e0b0f6077a308ec6f78d13caf0afc47046a842
Vladimir Zoric <vladimir.zorke@gmail.com>
2025-12-23 02:17:17 +0100

    feat: add Encounter aggregate and configure DTO mapping

---
+++ src/Modules/Encounters/Explorer.Encounters.API/Dtos/Encounter/CreateEncounterDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.API.Dtos.Encounter
+{
+    public class CreateEncounterDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        [JsonPropertyName("XP")]
+        public int XP { get; set; }
+        public string Type { get; set; }
+    }
+}
--- src/Modules/Encounters/Explorer.Encounters.Core/Mappers/EncountersProfile.cs
+++ src/Modules/Encounters/Explorer.Encounters.Core/Mappers/EncountersProfile.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Explorer.Encounters.API.Dtos.Encounter;
+using Explorer.Encounters.Core.Domain;
+using Explorer.Encounters.Core.Mappers.Converters;
 
 namespace Explorer.Encounters.Core.Mappers
 {
@@ -11,7 +14,24 @@ namespace Explorer.Encounters.Core.Mappers
     {
         public EncountersProfile()
         {
+            CreateMap<string, EncounterType>().ConvertUsing<EncounterTypeConverter>();
 
+            CreateMap<Encounter, EncounterDto>()
+                .ForMember(dest => dest.Latitude,
+                opt => opt.MapFrom(
+                    src => src.Location.Latitude))
+                .ForMember(dest => dest.Longitude,
+                opt => opt.MapFrom(
+                    src => src.Location.Longitude))
+                .ForMember(dest => dest.XP,
+                opt => opt.MapFrom(
+                    src => src.XP.Value));
+
+            CreateMap<CreateEncounterDto, Encounter>().ConstructUsing((src, ctx) => new Encounter(src.Name,
+                src.Description,
+                new BuildingBlocks.Core.Domain.GeoLocation(src.Latitude, src.Longitude),
+                new ExperiencePoints(src.XP),
+                ctx.Mapper.Map<EncounterType>(src.Type)));
         }
     }
 }
