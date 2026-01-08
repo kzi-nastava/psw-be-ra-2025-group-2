708c2afd73d17176939defec83e1fbb7850c866a
borh77 <borisahrnjez@gmail.com>
2026-01-08 13:36:12 +0100

    feat: add tour filters

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourEnums.cs
@@ -0,0 +1,56 @@
+namespace Explorer.Tours.Core.Domain;
+
+public enum TourStatus
+{
+    Draft,
+    Published,
+    Archived
+}
+
+public enum TourEnvironmentType
+{
+    Urban = 1,
+    Nature = 2,
+    Mixed = 3
+}
+
+public enum FoodType
+{
+    Vegetarian = 1,
+    Vegan = 2,
+    GlutenFree = 3,
+    LocalCuisine = 4,
+    FastFood = 5,
+    Finedining = 6,
+    StreetFood = 7
+}
+
+public enum AdventureLevel
+{
+    Low = 1,
+    Medium = 2,
+    High = 3
+}
+
+public enum ActivityType
+{
+    Adrenaline = 1,
+    Cultural = 2,
+    Relaxing = 3
+}
+
+public enum TransportType
+{
+    Walking = 0,
+    Bicycle = 1,
+    Car = 2
+}
+
+public enum SuitableFor
+{
+    Students = 1,
+    Children = 2,
+    Elderly = 3,
+    Families = 4,
+    Adults = 5
+}
