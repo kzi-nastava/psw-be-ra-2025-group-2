c758148a015cccd8c34aa4174b25a5ced4e4e4b3
borh77 <borisahrnjez@gmail.com>
2026-01-14 19:10:43 +0100

    feat: add dto

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/EstimatedCostItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Explorer.Tours.API.Dtos;
+
+public class EstimatedCostItemDto
+{
+    public int Category { get; set; }
+    public string CategoryName { get; set; } = "";
+
+    public decimal AmountPerPerson { get; set; }
+    public string Currency { get; set; } = "";
+    public string? Note { get; set; }
+
+}
