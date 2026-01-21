f4046d36d4625d25ed49a335772c294ce7fb30fe
borh77 <borisahrnjez@gmail.com>
2026-01-21 14:39:20 +0100

    feat: add dtos and service

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/Emergency/EmergencyOverviewResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.API.Dtos.Emergency
+{
+
+    public class EmergencyOverviewResponseDto
+    {
+        public string CountryCode { get; set; } = "";
+        public List<EmergencyPlaceDto> Hospitals { get; set; } = new();
+        public List<EmergencyPlaceDto> PoliceStations { get; set; } = new();
+        public string Instructions { get; set; } = "";
+        public string Disclaimer { get; set; } = "";
+    }
+}
