4e1d70a68967a977a604fa101d423d3d59efa78e
Vladimir Zoric <vladimir.zorke@gmail.com>
2026-01-24 08:57:26 +0100

    feat: define DTOs and service methods

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/Planner/SuggestionsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.API.Dtos.Planner
+{
+    public class SuggestionsDto
+    {
+        public int Month { get; set; }
+        public int? Day { get; set; }
+        List<string> Suggestions { get; set; } = new();
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/PlanerSuggestionMetadataDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class PlanerSuggestionMetadataDto
+    {
+        long TourId { get; set; }
+        string TourName { get; set; }
+        int TotalDurationMinutes { get; set; }
+        double FirstKeyPointLatitude { get; set; }
+        double FirstKeyPointLongitude { get; set; }
+    }
+}
