bd60d5276951f3910669a86a29018f73212baf63
Anđela Pinjić <pinjic.a276@gmail.com>
2026-01-28 02:12:40 +0100

    feat: add club member ranking based on XP

---
+++ src/Modules/Encounters/Explorer.Encounters.API/Internal/IInternalTouristProgressService.cs
@@ -0,0 +1,14 @@
+﻿using Explorer.Encounters.API.Dtos.Encounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.API.Internal
+{
+    public interface IInternalTouristProgressService
+    {
+        List<UserXpDto> GetXpForUsers(IEnumerable<long> userIds);
+    }
+}
